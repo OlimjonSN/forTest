6e97a420afb5c937c8ade4275871537a804c572a
olimjonsn233 <olimjonsn233@gmail.com>
2022-05-28 10:08:46 +0500

    test

---
+++ test/test_main.dart
@@ -0,0 +1,8 @@
+import '../bin/main.dart' as prety;
+import 'package:test/test.dart';
+
+void main() {
+  test("calculate", () {
+    expect(prety.func(40, 2), 42);
+  });
+}
--- testfunc/bin/main.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  print(func(5, 6));
-}
-
-int func(a, b) {
-  return a + b;
-}
